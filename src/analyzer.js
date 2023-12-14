b8382d1ca36a32f2e227678ab7bc0d730503c5ba
Maria Villalobos <mariavillalobos@Leonardos-MacBook-Pro-2.local>
2023-12-14 14:31:11 -0600

    Javascript wordCount, characterCount functions & addEventListener input textarea

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,9 +1,13 @@
 const analyzer = {  
   getWordCount: (text) => {
-    //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    const arr =text.trim().split(' ');
+    const arrFiltrado = arr.filter(word => word !== '')
+
+    return arrFiltrado.length;
   },
   getCharacterCount: (text) => {
-    //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+    const arr =text.split(' ');
+    return arr.length;
   },
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
--- src/index.js
+++ src/index.js
@@ -7,3 +7,20 @@ document.addEventListener('DOMContentLoaded', function() {
         textarea.value = '';
     })
 });
+
+/*Lo creé con un alert para saber que funciona, pero cuando cree las funciones para las métricas, entonces no debería desplegar nada.*/
+const textarea = document.querySelector('textarea[name="user-input"]')
+
+let datatestid = document.querySelector('data-testid')
+
+document.querySelector('textarea[name="user-input"]').addEventListener('input', function(evt){
+    const countWord= analyzer.getWordCount(textarea.value);
+    datatestid = document.querySelector('li[data-testid="word-count"]');
+    datatestid.innerHTML = "Palabras " + countWord;
+
+    const characterCount= analyzer.getCharacterCount(textarea.value);
+    datatestid = document.querySelector('li[data-testid="character-count"]');
+    datatestid.innerHTML = "Caracteres " + characterCount;
+
+});
+
