794147567f5d8a1e2e72f10cd03ac60e1aa172a4
Maria <maria.villaloboschaves@gmail.com>
2024-01-03 17:03:17 -0600

    Updates

--- analyzer.js
+++ analyzer.js
@@ -1,21 +1,60 @@
-const analyzer = {  
+const analyzer = {
   getWordCount: (text) => {
-    //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    text = text.replace(/[^\w\s]/g, '')
+    const arr = text.trim().split(' ');
+    const arrFiltrado = arr.filter(word => word !== '');
+    return arrFiltrado.length;
   },
   getCharacterCount: (text) => {
-    //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+    const arr = text.split('');
+    return arr.length;
   },
   getCharacterCountExcludingSpaces: (text) => {
-    //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+    text = text.replace(/[^\w\s]/g, '')
+    const arr = text.split('');
+    const arrFiltrado = arr.filter(chart => chart !== ' ');
+    return arrFiltrado.length;
   },
-  getAverageWordLength: (text) => {    
-    //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+  getCharacterCountExcludingSpacesIncludingSpecialCharacters: (text) => {
+    const arr = text.split('');
+    const arrFiltrado = arr.filter(chart => chart !== ' ');
+    return arrFiltrado.length;
+  },
+  getAverageWordLength: (text) => {
+    const valor1 = analyzer.getCharacterCountExcludingSpacesIncludingSpecialCharacters(text);
+    const valor2 = analyzer.getWordCount(text);
+    const division = valor1 / valor2;
+
+    return parseFloat(division.toFixed(2))
   },
   getNumberCount: (text) => {
-    //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+    let numberList = text.split(' ');
+    const pattern = /^-?\d*\.?\d+$/;
+    numberList = numberList.filter(x => {
+      x = x.replace(/\.$/, '')
+      if (pattern.test(x)) {
+        return true;
+      } else {
+        return false;
+      }
+    })
+
+    return numberList.length;
   },
   getNumberSum: (text) => {
-    //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+    const words = text.split(' ');
+    let sumaTotal = 0
+    const pattern = /^-?\d*\.?\d+$/;
+    for (let i = 0; i < words.length; i++) {
+      const item = words[i].replace(/\.$/, '')
+      if (pattern.test(item)) {
+        sumaTotal = sumaTotal + parseFloat(item);
+        console.log('sumaTotal', sumaTotal)
+      }
+    }
+
+    return sumaTotal;
+
   },
 };
 
--- index.js
+++ index.js
@@ -1,3 +1,40 @@
 import analyzer from './analyzer.js';
+document.addEventListener('DOMContentLoaded', function() {
+  const textarea = document.querySelector('textarea[name="user-input"]');
+  const eraseButton = document.getElementById('reset-button');
 
-//TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+  eraseButton.addEventListener('click', function() {
+    textarea.value = '';
+
+  })
+});
+
+const textarea = document.querySelector('textarea[name="user-input"]')
+
+document.querySelector('textarea[name="user-input"]').addEventListener('input', function() {
+  let datatestid;
+
+  const countWord= analyzer.getWordCount(textarea.value);
+  datatestid = document.querySelector('li[data-testid="word-count"]');
+  datatestid.innerHTML = "Palabras " + countWord;
+
+  const characterCount = analyzer.getCharacterCount(textarea.value);
+  datatestid = document.querySelector('li[data-testid="character-count"]');
+  datatestid.innerHTML = "Caracteres: " + characterCount;
+
+  const characterCountExclSpaces = analyzer.getCharacterCountExcludingSpaces(textarea.value);
+  datatestid = document.querySelector('li[data-testid="character-no-spaces-count"]');
+  datatestid.innerHTML = "Caracteres sin espacios: " + characterCountExclSpaces;
+
+  const wordAverageLength = analyzer.getAverageWordLength(textarea.value);
+  datatestid = document.querySelector('li[data-testid="word-length-average"]');
+  datatestid.innerHTML = "Promedio de longitud: " + wordAverageLength;
+
+  const numberCount = analyzer.getNumberCount(textarea.value);
+  datatestid = document.querySelector('li[data-testid="number-count"]');
+  datatestid.innerHTML = "Números: " + numberCount;
+
+  const numSum = analyzer.getNumberSum(textarea.value);
+  datatestid = document.querySelector('li[data-testid="number-sum"]');
+  datatestid.innerHTML = "Suma de números: " + numSum;
+});
