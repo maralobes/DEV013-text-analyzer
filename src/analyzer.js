b754f030cb68f12cdeaefdd9ac89eb69e2fec617
Maria Villalobos <mariavillalobos@Leonardos-MacBook-Pro-2.local>
2024-01-03 10:26:43 -0600

    Javascript numberCount & numSum

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,33 +1,54 @@
-const analyzer = {  
+const analyzer = {
   getWordCount: (text) => {
-    const arr =text.trim().split(' ');
+    const arr = text.trim().split(' ');
     const arrFiltrado = arr.filter(word => word !== '');
     return arrFiltrado.length;
   },
   getCharacterCount: (text) => {
-    const arr =text.split('');
+    const arr = text.split('');
     return arr.length;
   },
   getCharacterCountExcludingSpaces: (text) => {
-    const arr =text.split('');
-    const arrFiltrado =arr.filter(word => word !== ' ');
+    const arr = text.split('');
+    const arrFiltrado = arr.filter(word => word !== ' ');
     return arrFiltrado.length;
   },
-  getAverageWordLength: (text) => {    
+  getAverageWordLength: (text) => {
     const valor1 = analyzer.getCharacterCountExcludingSpaces(text);
     const valor2 = analyzer.getWordCount(text);
     const division = valor1 / valor2;
+
     return division.toFixed(2);
   },
   getNumberCount: (text) => {
-    const arrNumeros = text.replace(/[^0-9]/g,'');
-    return arrNumeros.length;
+    const stringOnlyNumbers = text.replace(/[^0-9\s]/g, '');
+    let numberList = stringOnlyNumbers.split(' ');
+    numberList = numberList.filter(x => {
+      if (x !== '') {
+        return true;
+      } else {
+        return false;
+      }
+    })
+
+    return numberList.length;
   },
   getNumberSum: (text) => {
-    const arrNumeros = text.replace(/[^0-9]/g,'');
-    const sumaTotal = 0
-    for (let i = 0; i < arrNumeros.length; i++){
-      sumaTotal = sumaTotal+arrNumeros.length;
+    const stringOnlyNumbers = text.replace(/[^0-9\s]/g, '');
+    let numberList = stringOnlyNumbers.split(' ');
+    numberList = numberList.filter(x => {
+      if (x !== '') {
+        return true;
+      } else {
+        return false;
+      }
+    })
+    console.log('numberList', numberList)
+
+    let sumaTotal = 0
+    for (let i = 0; i < numberList.length; i++) {
+      sumaTotal = sumaTotal + parseInt(numberList[i]);
+      console.log ('sumaTotal', sumaTotal)
     }
 
     return sumaTotal;
--- src/index.js
+++ src/index.js
@@ -1,46 +1,40 @@
 import analyzer from './analyzer.js';
 document.addEventListener('DOMContentLoaded', function() {
-    const textarea = document.querySelector('textarea[name="user-input"]');
-    const eraseButton = document.getElementById('reset-button');
+  const textarea = document.querySelector('textarea[name="user-input"]');
+  const eraseButton = document.getElementById('reset-button');
 
-    eraseButton.addEventListener('click', function () {
-        textarea.value = '';
-    })
+  eraseButton.addEventListener('click', function() {
+    textarea.value = '';
+
+  })
 });
 
-let textarea = document.querySelector('textarea[name="user-input"]')
+const textarea = document.querySelector('textarea[name="user-input"]')
 
-document.querySelector('textarea[name="user-input"]').addEventListener('input', function(evt){
-    let datatestid;
+document.querySelector('textarea[name="user-input"]').addEventListener('input', function() {
+  let datatestid;
 
-    const countWord= analyzer.getWordCount(textarea.value);
-    datatestid = document.querySelector('li[data-testid="word-count"]');
-    console.log(datatestid)
-    datatestid.innerHTML = "Palabras " + countWord;
+  const countWord= analyzer.getWordCount(textarea.value);
+  datatestid = document.querySelector('li[data-testid="word-count"]');
+  datatestid.innerHTML = "Palabras " + countWord;
 
-    const characterCount = analyzer.getCharacterCount(textarea.value);
-    datatestid = document.querySelector('li[data-testid="character-count"]');
-    console.log(datatestid)
-    datatestid.innerHTML = "Caracteres: " + characterCount;
+  const characterCount = analyzer.getCharacterCount(textarea.value);
+  datatestid = document.querySelector('li[data-testid="character-count"]');
+  datatestid.innerHTML = "Caracteres: " + characterCount;
 
-    const characterCountExclSpaces = analyzer.getCharacterCountExcludingSpaces(textarea.value);
-    datatestid = document.querySelector('li[data-testid="character-no-spaces-count"]');
-    console.log(datatestid)
-    datatestid.innerHTML = "Caracteres sin espacios: " + characterCountExclSpaces;
+  const characterCountExclSpaces = analyzer.getCharacterCountExcludingSpaces(textarea.value);
+  datatestid = document.querySelector('li[data-testid="character-no-spaces-count"]');
+  datatestid.innerHTML = "Caracteres sin espacios: " + characterCountExclSpaces;
 
-    const wordAverageLength = analyzer.getAverageWordLength(textarea.value);
-    datatestid = document.querySelector('li[data-testid="word-length-average"]');
-    console.log(datatestid)
-    datatestid.innerHTML = "Promedio de longitud: " + wordAverageLength;
+  const wordAverageLength = analyzer.getAverageWordLength(textarea.value);
+  datatestid = document.querySelector('li[data-testid="word-length-average"]');
+  datatestid.innerHTML = "Promedio de longitud: " + wordAverageLength;
 
-    const numberCount = analyzer.getNumberCount(textarea.value);
-    datatestid = document.querySelector('li[data-testid="number-count"]');
-    console.log(datatestid)
-    datatestid.innerHTML = "Números: " + numberCount;
+  const numberCount = analyzer.getNumberCount(textarea.value);
+  datatestid = document.querySelector('li[data-testid="number-count"]');
+  datatestid.innerHTML = "Números: " + numberCount;
 
-    const numSum = analyzer.getNumberSum(textarea.value);
-    datatestid = document.querySelector('li[data-testid="number-sum"]');
-    console.log(datatestid)
-    datatestid.innerHTML = "Suma de números: " + numSum;
+  const numSum = analyzer.getNumberSum(textarea.value);
+  datatestid = document.querySelector('li[data-testid="number-sum"]');
+  datatestid.innerHTML = "Suma de números: " + numSum;
 });
-
