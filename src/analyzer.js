9a8cc7e81d1e341aebc67b64a79ee4e45740b0c7
Maria <maria.villaloboschaves@gmail.com>
2024-01-03 18:29:51 -0600

    Readme

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,5 +1,6 @@
 const analyzer = {
   getWordCount: (text) => {
+    text = text.replace(/[^\w\s]/g, '')
     const arr = text.trim().split(' ');
     const arrFiltrado = arr.filter(word => word !== '');
     return arrFiltrado.length;
@@ -9,22 +10,29 @@ const analyzer = {
     return arr.length;
   },
   getCharacterCountExcludingSpaces: (text) => {
+    text = text.replace(/[^\w\s]/g, '')
     const arr = text.split('');
-    const arrFiltrado = arr.filter(word => word !== ' ');
+    const arrFiltrado = arr.filter(chart => chart !== ' ');
+    return arrFiltrado.length;
+  },
+  getCharacterCountExcludingSpacesIncludingSpecialCharacters: (text) => {
+    const arr = text.split('');
+    const arrFiltrado = arr.filter(chart => chart !== ' ');
     return arrFiltrado.length;
   },
   getAverageWordLength: (text) => {
-    const valor1 = analyzer.getCharacterCountExcludingSpaces(text);
+    const valor1 = analyzer.getCharacterCountExcludingSpacesIncludingSpecialCharacters(text);
     const valor2 = analyzer.getWordCount(text);
     const division = valor1 / valor2;
 
-    return division.toFixed(2);
+    return parseFloat(division.toFixed(2))
   },
   getNumberCount: (text) => {
-    const stringOnlyNumbers = text.replace(/[^0-9\s]/g, '');
-    let numberList = stringOnlyNumbers.split(' ');
+    let numberList = text.split(' ');
+    const pattern = /^-?\d*\.?\d+$/;
     numberList = numberList.filter(x => {
-      if (x !== '') {
+      x = x.replace(/\.$/, '')
+      if (pattern.test(x)) {
         return true;
       } else {
         return false;
@@ -34,21 +42,14 @@ const analyzer = {
     return numberList.length;
   },
   getNumberSum: (text) => {
-    const stringOnlyNumbers = text.replace(/[^0-9\s]/g, '');
-    let numberList = stringOnlyNumbers.split(' ');
-    numberList = numberList.filter(x => {
-      if (x !== '') {
-        return true;
-      } else {
-        return false;
-      }
-    })
-    console.log('numberList', numberList)
-
+    const words = text.split(' ');
     let sumaTotal = 0
-    for (let i = 0; i < numberList.length; i++) {
-      sumaTotal = sumaTotal + parseInt(numberList[i]);
-      console.log ('sumaTotal', sumaTotal)
+    const pattern = /^-?\d*\.?\d+$/;
+    for (let i = 0; i < words.length; i++) {
+      const item = words[i].replace(/\.$/, '')
+      if (pattern.test(item)) {
+        sumaTotal = sumaTotal + parseFloat(item);
+      }
     }
 
     return sumaTotal;
